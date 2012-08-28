030b2092501aaf51ce930df102849f987dc808e8
Lokinoid <lokinoid@gmail.com>
2012-08-28 17:56:39 +0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '5b6caacd83392ee66c56f1dbd15365e048cfd0daf920a5aa268cd9a6008a70a061b06fc03e13b99e58ba463bb34bc0c92e1d3431027d0959ead19517bcab0b0a'
